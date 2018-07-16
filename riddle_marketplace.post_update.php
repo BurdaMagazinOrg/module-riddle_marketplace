15da4d40c2a220266fc1fc956b255c39fea05cc4
Volker Killesreiter <killesre@gmail.com>
2018-07-16 10:10:21 +0200

    fix post update

--- riddle_marketplace.post_update.php
+++ riddle_marketplace.post_update.php
@@ -9,11 +9,12 @@
  * Set new api url if needed.
  */
 function riddle_marketplace_post_update_api_url() {
-  $module_settings = Drupal::configFactory()->get('riddle_marketplace.settings');
+  $module_settings = Drupal::configFactory()->getEditable('riddle_marketplace.settings');
   $api_url = $module_settings->get('riddle_marketplace.api_url');
   $new_api_url = 'https://www.riddle.com/api/creators/riddle/get-list?token=%%TOKEN%%';
 
   if ($api_url != $new_api_url) {
     $module_settings->set('riddle_marketplace.api_url', $new_api_url);
+    $module_settings->save();
   }
 }
